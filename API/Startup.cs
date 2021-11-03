4752b104fe4d31169680d90cd39d872bda2961d0
Jeff Whittington <jeff@codecology.com>
2021-11-03 16:52:52 -0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-
+            services.AddCors();
 //services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
@@ -52,6 +52,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
